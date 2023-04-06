bb8cf794a3b8f747e581f1daab3a47748d024d88
Mohandas T S <mohandass.unni3@gmail.com>
2023-04-06 13:18:24 +0530

    Truncate Sentence

---
+++ 1816.js
@@ -0,0 +1,22 @@
+// Turncate Sentence
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    let arr=s.split(" ")
+    s1=""
+    for(let i=0;i<arr.length;i++){
+        if(i<k){
+          s1 +=`${arr[i]} `
+        }
+    }
+    return s1.trim()
+
+
+    
+};
+
+console.log(truncateSentence("Hello how are you Contestant", 4));
