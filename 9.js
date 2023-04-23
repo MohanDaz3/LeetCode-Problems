28cffa985e40ec8bd6725e2bedfc43da9ff9703d
Mohandas T S <mohandass.unni3@gmail.com>
2023-04-23 19:42:45 +0530

    9. Palindrome Number

---
+++ 9.js
@@ -0,0 +1,22 @@
+// 9. Palindrome Number
+// link:https://leetcode.com/problems/palindrome-number/
+// not pushed to git
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+     x=String(x)
+     reverse=''
+    for(let i=x.length-1;i>=0;i--){
+        reverse+=x[i]
+    }
+    if(x==reverse){
+        return true
+    }else{
+        return false
+    }
+    
+};
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
