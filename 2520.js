52e20bd6325e8966d4ab22bda45decbacafa0229
Mohandas T S <mohandass.unni3@gmail.com>
2023-05-25 09:38:03 +0530

    2520. Count the Digits That Divide a Number

---
+++ 2520.js
@@ -0,0 +1,23 @@
+// 2520. Count the Digits That Divide a Number
+// link:https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countDigits = function(num) {
+let count=0
+num=String(num)
+// console.log(num);
+for(let i=0;i<num.length;i++){
+    if(num%num[i]===0){
+        count++
+    }
+}
+return count
+    
+
+};
+console.log(countDigits(7));
+console.log(countDigits(121));
+console.log(countDigits(1248));
